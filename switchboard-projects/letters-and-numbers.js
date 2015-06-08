cdb155e99d6ff1cba3061828e9cb45ac98020dcf
brettimus <brbeut@gmail.com>
2015-06-08 05:15:48 -0700

    souped up letter and number getter

--- switchboard-projects/letters-and-numbers.js
+++ switchboard-projects/letters-and-numbers.js
@@ -1,7 +1,9 @@
+var dict = Object.create(null);
 var lower_case = [];
 var upper_case = [];
 var numbers = [];
-var symbols = "\' \" , . : { } [ ] % - _".split(" ");
+var symbols = "\' \" , . : { } [ ] % - _ \n \r".split(" ");
+symbols.push(" "); // add a space :D
 
 var aCode = "a".charCodeAt(0);    // get the unicode code point for lower case a
 var ACode = "A".charCodeAt(0);    // samesies for upper case A
@@ -13,6 +15,13 @@ range(0,25).forEach(function(i) {
 });
 range(0,9).forEach(function(i) {
     numbers.push(Sring.fromCharCode(zeroCode+i));
+});
+
+// alternatively
+range(0, 25).forEach(function(i) {
+    if (i < 10) dict[Sring.fromCharCode(zeroCode+i)] = true;
+    dict[String.fromCharCode(aCode+i)] = true;
+    dict[String.fromCharCode(ACode+i)] = true;
 });
 
 function range(a, b) {
